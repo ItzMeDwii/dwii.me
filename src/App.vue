3d6c671c4c4030e0553534fca2cce948e8dc7add
ItzMeDwii <dwii5359@azalelnation.com>
2020-06-19 11:07:43 +0700

    Change Navbar.

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <!--
-       * router-link tag is used to move the router-view
-      -->
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      <!--
-       * we include LocaleChanger component to change locales directly into the root
-      -->
-      
-      <LocaleChanger/>
+      <nav class="py-2 navbar fixed-top navbar-expand-lg navbar-dark default-color">
+        <div class="container">
+          <a class="navbar-brand font-weight-bold" href="/"> DwiiUnknown </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#"><router-link to="/">Home</router-link></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><router-link to="/about">About</router-link></a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ml-auto">
+            <ThemeChanger/>
+            <LocaleChanger/>
+          </ul>
+        </div>
+        </div>
+      </nav>
     </div>
     <!--
      * This is where your content will be displayed.
