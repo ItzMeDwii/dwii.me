63823fe71cf3c822dbb593afd487b04fd4e5803b
ItzMeDwii <harnowo991943@gmail.com>
2020-10-06 20:41:22 +0700

    chore: update content & meta properties

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -16,13 +16,16 @@ export default class Home extends Vue {
             <h1 class="text-center text-white font-weight-bold my-1">DwiiUnknown</h1>
 
             <p class="text-center text-white my-2">
-              Satya Bagus Dwiatmaja • 15 y.o. • DwiiUnknown#3704<br />
-    I'm a Front-end Developer, i also like to make discord bots. Currently Developer at Auron Empire Minecraft Server & Mizu Development.
+              Satya Bagus Dwiatmaja • 16 y.o. • DwiiUnknown#3704<br />
+    I'm a Front-end Developer, i also like to make discord bots. Currently Developer at Auron Empire Minecraft Server & Mizu Development.<br />
+    This website is made from Vue + Typescript and Material Design Bootstrap, you can check the source code <a href="https://github.com/ItzMeDwii/dwii.me">here</a>.
     </p>
 
 
             <div class="row d-flex justify-content-center my-4" role="group">
-              <a href="https://github.com/ItzMeDwii/" class="btn text-white waves-efect font-weight-bolder">My Github</a>
+              <a href="https://github.com/ItzMeDwii/" target="_blank" class="btn text-white waves-efect font-weight-bolder">My Github</a>
+              <a href="https://auronempire.com" class="btn text-white waves-efect font-weight-bolder">Auron Empire</a>
+              <a href="https://mizudev.net" class="btn text-white waves-efect font-weight-bolder">Mizu Dev</a>
             </div>
           </div>
         </div>
